c35d48c2444e1048fc821b13441ab855731ac1cf
Baptiste Perbos <baptiste.perbos@gmail.com>
2018-02-11 14:39:33 +0100

    add jwt signin support

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-import _ from './env';
+import './env';
 import express from 'express';
 import bodyParser from 'body-parser';
-import jwt from 'express-jwt';
+import expressJwt from 'express-jwt';
+import jwt from 'jsonwebtoken';
+
+const PORT = process.env.PORT || 3000;
 
 let app = express();
 
 app.use(bodyParser.json())
+app.use(expressJwt({ secret: process.env.SECRET}).unless({path: ['/signin']}));
 
 app.get('/', function(req, res) {
     res.send('Hello World');
 })
 
-let server = app.listen(3000, function() {
+app.get('/signin', function(req, res) {
+    const token = jwt.sign(
+        { /* payload */ },
+        process.env.SECRET,
+        { expiresIn: 24 * 60 * 60 }
+    );
+    res.status(200).send({
+            token: token
+        })
+})
+
+let server = app.listen(PORT, function() {
     console.log(`Server running at http://localhost:${server.address().port}`);
 })
